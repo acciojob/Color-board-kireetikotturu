72e68995e10c55e00139df452435fb98cf039019
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-09-02 16:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,16 +6,16 @@
 		box.classList.add("square");
 		cont.appendChild(box);
 
-		let r = Math.floor(Math.random() * 255);
-		let g = Math.floor(Math.random() * 255);
-		let b = Math.floor(Math.random() * 255);
-
 		box.addEventListener("mouseenter", function () {
+			const r = Math.floor(Math.random() * 255);
+			const g = Math.floor(Math.random() * 255);
+			const b = Math.floor(Math.random() * 255);
 			box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 		});
 
 		box.addEventListener("mouseleave", function(){
-			box.style.backgroundColor = "rgb(29, 29, 29)"
-			box.style.transition = "background-color 1s ease"
+			setTimeout(function() {
+				box.style.backgroundColor = "rgb(29, 29, 29)"
+			}, 1000)
 		});
 	}
