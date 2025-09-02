1ad291b6349bac84036dcbff571d4bc009030b9e
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-09-02 16:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,12 +15,7 @@
 		});
 
 		box.addEventListener("mouseleave", function(){
-			setTimeout(() => {
-				box.style.backgroundColor = "rgb(29, 29, 29)"
-				box.style.transition = "background-color 1s ease"
-			}, 1000);
-			
-			});
-
-
+			box.style.backgroundColor = "rgb(29, 29, 29)"
+			box.style.transition = "background-color 1s ease"
+		});
 	}
